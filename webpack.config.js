46a4e828c8f57e0c8e21279207e0d23289849b8e
shr1ftyy <syeamtechdemon@gmail.com>
2024-08-13 15:19:29 +0930

    feat: updated preprocessing code - works fine now

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,31 @@
 // webpack.config.js
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: {
-    preprocess: "./src/preprocess.js",
-    postprocess: "./src/postprocess.js",
-  },
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  mode: "production",
+    entry: {
+        preprocess: "./src/preprocess.js",
+        postprocess: "./src/postprocess.js",
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    mode: "production",
+    resolve: {
+        extensions: [".ts", ".js"],
+        fallback: {
+            buffer: require.resolve("buffer"),
+        },
+    },
+    plugins: [
+        // Work around for Buffer is undefined:
+        // https://github.com/webpack/changelog-v5/issues/10
+        new webpack.ProvidePlugin({
+            Buffer: ["buffer", "Buffer"],
+        }),
+        new webpack.ProvidePlugin({
+            process: "process/browser",
+        }),
+    ],
 };
