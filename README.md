2cc573ee3d06d1cd927ba74df9bc4429ec2d8869
shr1ftyy <syeamtechdemon@gmail.com>
2024-08-17 07:21:52 +0000

    feat: postprocesing script done

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Sturdy Subnet Airnode Preprocessing and Postprocessing
+
 This repository contains the Javascript code which is bundled to be used as preprocessing and postprocessing
 functions for Sturdy Subnet's airnode.
 
 # Usage
+
 ```bash
 # install node modules
 npm install
@@ -11,3 +13,18 @@ npm run bundle
 # bundle js + format into an escaped json-friendly string to drop into airnode config.json
 npm run bundleformat
 ```
+
+**NOTE:** You may have to manually modify the bundled and formatted js to include parameters
+`input`, `output`, and other airnode-relevant parameters. For example you may have to modify the formatted and bundled js for `postprocess.js` from:
+
+```js
+...s=function(e){let t=e.request_uuid;t=t.length>32?t.slice(0,32):t.padEnd(32,\" \");const r=n.hexlify(n.toUtf8Bytes(t)),s=Object.keys(e.allocations)[0],i=e.allocations[s],o=s,a=Object.keys(i.allocations),c=Object.values(i.allocations).map((e=>BigInt(e)));return console.log(`request_uuid: \\${r}`),console.log(`minerUID: \\${o}`),console.log(`addresses: \\${a}`),console.log(`allocations: \\${c}`),(new n.AbiCoder).encode([\"bytes32\",\"uint256\",\"address[]\",\"uint256[]\"],[r,o,a,c])}(input);console.log(\"Encoded Data:\",s)...
+```
+
+to:
+
+```js
+...s=function(e){let t=e.request_uuid;t=t.length>32?t.slice(0,32):t.padEnd(32,\" \");const r=n.hexlify(n.toUtf8Bytes(t)),s=Object.keys(e.allocations)[0],i=e.allocations[s],o=s,a=Object.keys(i.allocations),c=Object.values(i.allocations).map((e=>BigInt(e)));return console.log(`request_uuid: \\${r}`),console.log(`minerUID: \\${o}`),console.log(`addresses: \\${a}`),console.log(`allocations: \\${c}`),(new n.AbiCoder).encode([\"bytes32\",\"uint256\",\"address[]\",\"uint256[]\"],[r,o,a,c])}(input);console.log(\"Encoded Data:\",s);output=s...
+```
+
+(added `;output=s`)
--- src/postprocess.js
+++ src/postprocess.js
@@ -1,13 +1,84 @@
-// const { ethers } = require("ethers");
+const { ethers } = require("ethers");
 
+function processAndEncode(input) {
+    // Extract the request UUID
+    let inputString = input.request_uuid;
+    if (inputString.length > 32) {
+        inputString = inputString.slice(0, 32);
+    } else {
+        inputString = inputString.padEnd(32, " "); // Padding with spaces or your preferred character
+    }
+
+    const request_uuid = ethers.hexlify(ethers.toUtf8Bytes(inputString));
+
+    // Extract the top miner (first allocation in the object)
+    const topMinerKey = Object.keys(input.allocations)[0];
+    const topMiner = input.allocations[topMinerKey];
+
+    // Extract the miner UID, addresses, and allocations
+    const minerUID = topMinerKey;
+    const addresses = Object.keys(topMiner.allocations);
+    const allocations = Object.values(topMiner.allocations).map((allocation) =>
+        BigInt(allocation),
+    );
+
+    console.log(`request_uuid: ${request_uuid}`);
+    console.log(`minerUID: ${minerUID}`);
+    console.log(`addresses: ${addresses}`);
+    console.log(`allocations: ${allocations}`);
+
+    // Encode all data together
+    const abiCoder = new ethers.AbiCoder();
+    const encodedData = abiCoder.encode(
+        ["bytes32", "uint256", "address[]", "uint256[]"],
+        [request_uuid, minerUID, addresses, allocations],
+    );
+
+    return encodedData;
+}
+
+// Example input:
 // input = {
-//     request_uuid: "test69",
-//     allocations: Dict[str, AllocInfo] = Field(
-//         ...,
-//         description="allocations produce by miners",
-//     )
-// }
+//     request_uuid: "3ed28066e4ad46abb292f7e64dccaa8a",
+//     allocations: {
+//         1: {
+//             apy: 0,
+//             allocations: {
+//                 "0xe53FFd56FaDC7030156069aE1b34dE0Ab8b703F4": 4.07e21,
+//                 "0xC8D4a8a7F593e73cD32cD6C5Fb11fE20F23f9695": 0,
+//                 "0xD002Dc1c05fd7FF28C55eEA3dDcB9051B2B81bD2": 0,
+//                 "0x0DD49C449C788285F50B529145D6e6E76f02Fd8f": 0,
+//             },
+//         },
+//         2: {
+//             apy: 0,
+//             allocations: {
+//                 "0xe53FFd56FaDC7030156069aE1b34dE0Ab8b703F4": 4.07e21,
+//                 "0xC8D4a8a7F593e73cD32cD6C5Fb11fE20F23f9695": 0,
+//                 "0xD002Dc1c05fd7FF28C55eEA3dDcB9051B2B81bD2": 0,
+//                 "0x0DD49C449C788285F50B529145D6e6E76f02Fd8f": 0,
+//             },
+//         },
+//         4: {
+//             apy: 0,
+//             allocations: {
+//                 "0xe53FFd56FaDC7030156069aE1b34dE0Ab8b703F4": 4.07e21,
+//                 "0xC8D4a8a7F593e73cD32cD6C5Fb11fE20F23f9695": 0,
+//                 "0xD002Dc1c05fd7FF28C55eEA3dDcB9051B2B81bD2": 0,
+//                 "0x0DD49C449C788285F50B529145D6e6E76f02Fd8f": 0,
+//             },
+//         },
+//         9: {
+//             apy: 0,
+//             allocations: {
+//                 "0xe53FFd56FaDC7030156069aE1b34dE0Ab8b703F4": 4.07e21,
+//                 "0xC8D4a8a7F593e73cD32cD6C5Fb11fE20F23f9695": 0,
+//                 "0xD002Dc1c05fd7FF28C55eEA3dDcB9051B2B81bD2": 0,
+//                 "0x0DD49C449C788285F50B529145D6e6E76f02Fd8f": 0,
+//             },
+//         },
+//     },
+// };
 
-// abiCoder = new ethers.AbiCoder();
-// abiCoder.encode();
-console.log("uwu! :3");
+const output = processAndEncode(input);
+console.log("Encoded Data:", output);
